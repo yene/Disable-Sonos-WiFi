a4aa940756b1c3f4552b668584146c7de18ec159
Yannick Weiss <contact@yannickweiss.com>
2017-05-02 08:14:34 +0200

    Tested with acutal sonos.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Disable Sonos WiFi
 Disable sonos built in WiFi (and bridge) to prevent configuration mistakes.
 
-# TODO
+### TODO
 * Run search on all interfaces.
+
+### Default adapter names
+* darwin = en0
+* linux = eth0
+* windows = Ethernet
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"runtime"
 
+	"strings"
+
 	"github.com/ianr0bkny/go-sonos/ssdp"
 )
 
@@ -20,22 +22,24 @@ func main() {
 	result := mgr.QueryServices(qry)
 	if dev_list, has := result["schemas-upnp-org-ContentDirectory"]; has {
 		for _, dev := range dev_list {
-			log.Printf("%s %s %s %s %s\n", dev.Product(), dev.ProductVersion(), dev.Name(), dev.Location(), dev.UUID())
+			location := string(dev.Location())
+			address := strings.Replace(location, ":1400/xml/device_description.xml", "", 1)
+			ip := strings.Replace(address, "http://", "", 1)
+			log.Printf("Found %s %s\n", dev.Product(), ip)
 
 			// wifictrl request succeeded HTTP 200 OK
-			resp, err := http.Get("http://<sonos_ip>:1400/wifictrl?wifi=persist-off")
-			defer resp.Body.Close()
+			resp, err := http.Get(address + ":1400/wifictrl?wifi=persist-off")
 			if err != nil {
 				log.Println(err)
 			}
+			defer resp.Body.Close()
 			if resp.StatusCode == 200 {
-				log.Println("Disabled WiFi for", dev.Name())
+				log.Println("Disabled WiFi for", dev.Product(), ip)
 			}
 			break
 		}
 	}
 	mgr.Close()
-
 }
 
 func guessMainInterfaceName() string {
@@ -49,6 +53,7 @@ func guessMainInterfaceName() string {
 		for _, addr := range addrs {
 			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 				if ipnet.IP.To4() != nil {
+					log.Println("Searching on network adapter", i.Name)
 					return i.Name
 				}
 			}
@@ -58,5 +63,6 @@ func guessMainInterfaceName() string {
 	if runtime.GOOS == "darwin" {
 		return "en1"
 	}
+
 	return "eth0"
 }
